d8c2a6407b456bf5c8427028093be95b6929ee96
Aline Vianna <alinealvesvianna@gmail.com>
2017-02-05 19:17:29 -0200

    segunda etapa do projeto cat.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,71 @@
 (function() {
-  var contador = 0;
+    var containerGeral = document.getElementById('container');
 
-    function contarCliques() {
-        contador++;
-        var placar = document.getElementById('placarCliques');
-        placar.innerHTML = "já tenho "+ contador + " cliques";
+    var dadosGatos = [{
+        "titulo_gato": "esse é o primeiro gatinho",
+        "imagem_gato": "img/catImage.jpg"
+    }, {
+        "titulo_gato": "esse é o segundo gatinho",
+        "imagem_gato": "img/catImage2.jpg"
+    }];
+
+    function gerarContainerGatinhos(i) {
+        var containerGato,
+            tituloGato,
+            imagemGato,
+            contadorGato;
+
+        containerGato = document.createElement('div');
+        tituloGato = document.createElement('h1');
+        imagemGato = document.createElement('img');
+        contadorGato = document.createElement('p');
+
+        containerGato.classList.add('container-gato');
+        tituloGato.classList.add('titulo-gato');
+        imagemGato.classList.add('imagem-gato');
+        contadorGato.classList.add('txtContador');
+
+        tituloGato.id = "tituloGato" + i;
+        contadorGato.id = "placarCliques" + i;
+        imagemGato.id = "imagemGato" + i;
+        imagemGato.atributoClique = contadorGato.id;
+        imagemGato.contador = 0;
+
+        imagemGato.addEventListener('click', function() {
+            // contarCliques(imagemGato.atributoClique, imagemGato.contador)
+            var contador = imagemGato.contador += 1;
+            var placar = document.getElementById(imagemGato.atributoClique);
+            placar.innerHTML = "já tenho " + contador + " cliques";
+
+        });
+
+
+        containerGeral.appendChild(containerGato);
+        containerGato.appendChild(tituloGato);
+        containerGato.appendChild(imagemGato);
+        containerGato.appendChild(contadorGato);
+
+    };
+
+
+    for (var i = 0, j = dadosGatos.length; i < j; i++) {
+        gerarContainerGatinhos(i);
+        document.getElementById('tituloGato' + i).innerHTML = dadosGatos[i].titulo_gato;
+        document.getElementById('imagemGato' + i).src = dadosGatos[i].imagem_gato;
+
     }
-    document.getElementById('imagemContador').addEventListener("click", contarCliques)
+
+
+
+
+
+    // function contarCliques(id, contador) {
+    //     var contador = contador += 1;
+    //     var placar = document.getElementById(id);
+    //     placar.innerHTML = "já tenho " + contador + " cliques";
+    // }
+
+
 
 
 })()
